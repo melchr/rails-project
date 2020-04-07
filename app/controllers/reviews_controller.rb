97dd98e77f65145f7693782b129b290381474e4e
Mel <melchrstmn@gmail.com>
2020-04-07 19:10:45 -0400

    added show and new actions to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,10 @@ class ReviewsController < ApplicationController
     def index
         @reviews = Review.all
     end
+    def show
+        @review = Review.find(params[:id])
+    end
+    def new
+        @review = Item.new
+    end
 end
