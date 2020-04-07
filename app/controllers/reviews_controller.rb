b72f48c71d081951ce359fcebf22f14b892d5e6d
Mel <melchrstmn@gmail.com>
2020-04-07 19:29:08 -0400

    switched order of actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,10 +2,10 @@ class ReviewsController < ApplicationController
     def index
         @reviews = Review.all
     end
-    def show
-        @review = Review.find(params[:id])
-    end
     def new
         @review = Review.new
+    end
+    def show
+        @review = Review.find(params[:id])
     end
 end
