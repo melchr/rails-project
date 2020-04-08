f73c207e6a9610af1f8a346b0c7b423b45f2199f
Mel <melchrstmn@gmail.com>
2020-04-07 22:56:14 -0400

    edited routes, still not done

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { omniauth_callbacks: "users/omniauth_callbacks" }
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :albums
+  end
   resources :reviews do
     resources :comments
   end
 
-  root to: "home#index"
-  
+  root to: "albums#index"
+
 end
